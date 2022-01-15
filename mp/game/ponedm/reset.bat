30d89a5d99edaed742c03381222450915b8c9f6e
Bitl <bitlblox@gmail.com>
2022-01-15 13:26:57 -0700

    add hl2mp_shared contents

--- mp/game/ponedm/reset.bat
+++ mp/game/ponedm/reset.bat
@@ -7,19 +7,19 @@ del modelsounds.cache
 del hl2mp_shared.vpk.sound.cache
 del demoheader.tmp
 del textwindow_temp.html
+del console.log
 del cfg\config.cfg
 del cfg\settings.scr
 del cfg\user.scr
 del bin\client.pdb
 del bin\server.pdb
+del sound\sound.cache
 rmdir /S /Q maps\graphs\
+rmdir /S /Q materials\vgui\logos\ui\
 rmdir /S /Q download\user_custom\
-rmdir /S /Q materials\
-rmdir /S /Q models\
 rmdir /S /Q download\sound\
 rmdir /S /Q download\materials\
 rmdir /S /Q download\models\
 rmdir /S /Q downloadlists\
-COPY "..\..\..\README.md" README-AND-CREDITS.txt
 pause
 exit
---
+++ mp/game/vpk/vpk_HL2MP.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+"D:\SteamLibrary\steamapps\common\FIREFIGHT RELOADED\bin\vpk.exe" -M "G:\Projects\GitHub\ponedm-source-sdk-2013-ce\mp\game\vpk\hl2mp_shared"
+pause
