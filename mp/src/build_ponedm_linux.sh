3609798691b9fd1d8a16631e767504dc5a3c2b1a
Bitl <bitlblox@gmail.com>
2023-09-28 19:39:40 -0700

    attempting linux build

---
+++ mp/src/build_ponedm_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd -
+sudo git stash push --include-untracked
+sudo git stash drop
+sudo git pull origin main
+cd ./sp/src
+sudo chmod -R +x devtools/*
+sudo chmod -R +x devtools/bin
+sudo chmod -R +x devtools/bin/linux
+sudo chmod +x creategameprojects
+sudo bash creategameprojects
+sudo make -f Game_ponedm.mak
---
+++ mp/src/update_ponedm_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd -
+sudo git stash push --include-untracked
+sudo git stash drop
+sudo git pull origin main
